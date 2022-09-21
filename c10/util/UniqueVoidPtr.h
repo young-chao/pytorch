7240f4ce806892df1ae1dbf2b1c96a554ad02f26
young_chao <34190033+young-chao@users.noreply.github.com>
2022-09-21 21:49:55 +0800

    Update UniqueVoidPtr.h

--- c10/util/UniqueVoidPtr.h
+++ c10/util/UniqueVoidPtr.h
@@ -12,11 +12,14 @@ namespace detail {
 // Does not delete anything
 TORCH_API void deleteNothing(void*);
 
+// detail::UniqueVoidPtr是一个类似于unique_ptr的所有权智能指针，但有三个主要区别：
 // A detail::UniqueVoidPtr is an owning smart pointer like unique_ptr, but
 // with three major differences:
 //
+//    1) 专门用于void
 //    1) It is specialized to void
 //
+//    2) 专门用于函数指针删除器 void(void* ctx); 即，删除器不引用数据，只引用上下文指针（被删除为 void*）。 事实上，在内部，这个指针被实现为拥有对上下文的引用和对数据的非拥有引用； 这就是为什么您使用 release_context() 而不是 release() 的原因（用于 release() 的传统 API 不会为您提供足够的信息来稍后正确处理该对象。）
 //    2) It is specialized for a function pointer deleter
 //       void(void* ctx); i.e., the deleter doesn't take a
 //       reference to the data, just to a context pointer
@@ -27,6 +30,8 @@ TORCH_API void deleteNothing(void*);
 //       API for release() wouldn't give you enough information
 //       to properly dispose of the object later.)
 //
+//    3) 保证在唯一指针被销毁且上下文非空时调用deleter. 这与std::unique_ptr不同, 
+//       std::unique_ptr如果数据指针为空，则不调用删除器. 
 //    3) The deleter is guaranteed to be called when the unique
 //       pointer is destructed and the context is non-null; this is different
 //       from std::unique_ptr where the deleter is not called if the
@@ -91,6 +96,12 @@ class UniqueVoidPtr {
 
 // Note [How UniqueVoidPtr is implemented]
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// UniqueVoidPtr 解决了张量数据分配器的一个常见问题，即您感兴趣的数据指针（例如, float*）
+// 与您实际解除分配数据所需的上下文指针（例如，DLManagedTensor）不同. 在传统的删除器设计
+// 下，您必须在删除器本身中存储额外的上下文，以便您可以实际删除正确的内容。
+// 用标准 C++ 实现这个有点容易出错：如果使用 std::unique_ptr 来管理张量，如果数据指针为 
+// nullptr，则不会调用删除器，如果上下文指针为非null(并且删除器负责释放数据指针和上下文
+// 指针)，则可能导致泄漏. 
 // UniqueVoidPtr solves a common problem for allocators of tensor data, which
 // is that the data pointer (e.g., float*) which you are interested in, is not
 // the same as the context pointer (e.g., DLManagedTensor) which you need
@@ -102,6 +113,8 @@ class UniqueVoidPtr {
 // context pointer is non-null (and the deleter is responsible for freeing both
 // the data pointer and the context pointer).
 //
+// 因此，在我们重新实现的 unique_ptr 中，它就将上下文直接存储在唯一指针中，并将删
+// 除器附加到上下文指针本身。 在简单的情况下，上下文指针就是指针本身。
 // So, in our reimplementation of unique_ptr, which just store the context
 // directly in the unique pointer, and attach the deleter to the context
 // pointer itself.  In simple cases, the context pointer is just the pointer
