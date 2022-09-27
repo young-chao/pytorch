919d4a81852045b96193649c414813ac6306a91c
young_chao <34190033+young-chao@users.noreply.github.com>
2022-09-27 22:24:18 +0800

    Update graph_task.h

--- torch/csrc/autograd/graph_task.h
+++ torch/csrc/autograd/graph_task.h
@@ -15,22 +15,31 @@ struct ReadyQueue;
 static constexpr int NO_DEVICE = -2;
 static constexpr int CPU_DEVICE = -1;
 
+// GraphTask实例代表一个动态图级别的资源管理对象，拥有一次反向传播执行所需要的全部元数据。
+// 如果允许重入反向传播，则会有多个GraphTask一起工作。
 // GraphTask holds metadata needed for a single execution of backward()
 struct GraphTask : std::enable_shared_from_this<GraphTask> {
-  std::atomic<uint64_t> outstanding_tasks_{0};
+  std::atomic<uint64_t> outstanding_tasks_{0}; //用来记录当前任务数目，如果数目为0，则说明任务结束。 如果这个数量不为0，则此GraphTask依然需要运行。
   // Indicates if an error occurred while executing any task.  When this is
   // true, it signals all threads to stop executing.
-  std::atomic_bool has_error_{false};
+  std::atomic_bool has_error_{false}; //错误发生标识符
   std::atomic_bool future_completed_{false};
   // It is safe to read keep_graph_ without synchronization
-  bool keep_graph_;
+  bool keep_graph_; //用来指定一次反向计算后是否释放资源。
 
   // To protect reads/writes to not_ready_, dependencies_, captured_vars_,
   // has_error_, future_result_, cpu_ready_queue_, and leaf_streams.
-  std::mutex mutex_;
-  std::unordered_map<Node*, InputBuffer> not_ready_;
-  std::unordered_map<Node*, int> dependencies_;
+  std::mutex mutex_; //互斥量，用于保护对以上英文注释所提及变量的读写
+  std::unordered_map<Node*, InputBuffer> not_ready_; //针对未就绪节点和其输入的map。key是未就绪节点，value是这个节点目前就绪的输入列表。
+  std::unordered_map<Node*, int> dependencies_; //记录节点间的依赖，用来判断后续节点是否已经可以被执行。
 
+  /* Exec info允许过滤图上不需要的路径。
+     如果它为空，则意味着任务以默认模式运行，所有next_edges都应该被执行。 
+     如果它不为空，则仅执行exec_info[node].needed_=true的特定节点。
+     这些节点的性质是：节点拥有一条路径，这路径可以通往outputs的任何一条边。
+     exec_info仅在通过.backward()执行图形且未传递输入参数时为空。
+     当通过.grad()执行或为.backward()指定inputs时，exec_info将非空。
+     */
   // Note [Exec info]
   // Exec info is created for each GraphTask, which allows filtering paths on
   // the graph that are not needed. It has a bit complicated semantics. If it's
@@ -75,7 +84,7 @@ struct GraphTask : std::enable_shared_from_this<GraphTask> {
     std::unique_ptr<std::vector<Capture>> captures_;
   };
   // exec_info_ is safe to read without synchronization
-  std::unordered_map<Node*, ExecInfo> exec_info_;
+  std::unordered_map<Node*, ExecInfo> exec_info_; //执行信息，用于剪除不需要执行的分支
   // Captures variables are grads captured that we return to the user. After
   // execution of the GraphTask is completed, the captured_vars_ are moved
   // out of the GraphTask and are no longer valid.
@@ -103,7 +112,7 @@ struct GraphTask : std::enable_shared_from_this<GraphTask> {
   // The value of worker_device in the thread that created this task.
   // See Note [Reentrant backwards]
   // Safe to read owner_ and reentrant_depth_ without synchronizaton
-  int owner_;
+  int owner_; //GraphTask所属线程的Device数值。该值是创建GraphTask线程中的worker_device的值。
   // The number of parent graph tasks for this graph task
   const int reentrant_depth_;
 
@@ -131,12 +140,16 @@ struct GraphTask : std::enable_shared_from_this<GraphTask> {
   // an exception as soon as the autograd engine receives an exception.
   bool exit_on_error_;
 
+  /* CPU线程专用于处理反向传播之中的CPU相关工作。因此所有GraphTask都会维护自己的
+     cpu_ready_queue_，CPU相关任务应该发送到该队列。对于每个GraphTask，我们维护
+     cpu_ready_queue_，这样在设备线程（即GPU）上执行时，如果下一个NodeTask应该在
+     CPU上运行，我们就知道应该推送NodeTask到哪个就绪队列。*/
   // CPU threads are dedicated to processing CPU work for the backward they
   // invoked. So any given graph task maintains its own cpu_ready_queue_ where
   // you should send work for it to be done. We memoize the cpu_ready_queue_ per
   // GraphTask so that we know which ready queue we should push to if we are on
   // device thread (i.e. GPU) and but next NodeTask should be run on CPU.
-  std::shared_ptr<ReadyQueue> cpu_ready_queue_;
+  std::shared_ptr<ReadyQueue> cpu_ready_queue_; //CPU线程队列
 
   // Future representing the completion of the graph task. Notified when all
   // tasks are done.
