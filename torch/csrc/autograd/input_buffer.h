c401d2d948f92e3af3f27b44c180dff914ad0247
young_chao <34190033+young-chao@users.noreply.github.com>
2022-09-28 21:51:13 +0800

    Update input_buffer.h

--- torch/csrc/autograd/input_buffer.h
+++ torch/csrc/autograd/input_buffer.h
@@ -16,15 +16,18 @@
 namespace torch {
 namespace autograd {
 
+// 一些节点在反向计算时有多个输入。因此在计算梯度的时候，grad_fn的输入可能从
+// 很多条路径上累积过来，InputBuffer就是用来累加grad_fn的输入。
 struct InputBuffer {
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init)
-  explicit InputBuffer(size_t size) : buffer(size) {}
+  explicit InputBuffer(size_t size) : buffer(size) {} //size表示输入数量
   InputBuffer(const InputBuffer& other) = delete;
   InputBuffer(InputBuffer&& other) = default;
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init)
   explicit InputBuffer(variable_list&& inputs) : buffer(std::move(inputs)){};
   InputBuffer& operator=(InputBuffer&& other) = default;
 
+  // 累积指定索引处的variable
   // Accumulates the variable at a specified index.
   // The optional CUDA streams determine which stream the accumulation
   // is run on and how the addition is synchronized.
@@ -34,8 +37,9 @@ struct InputBuffer {
       const c10::optional<c10::Stream>& opt_producer_stream,
       const c10::optional<c10::Stream>& opt_consumer_stream);
 
-  at::Device device() const;
+  at::Device device() const; //获取输入张量的设备
 
+  // 重载[]用于取对应位置的梯度Variable
   Variable operator[](size_t pos) {
     return buffer[pos];
   }
@@ -44,7 +48,7 @@ struct InputBuffer {
   static std::vector<Variable> variables(InputBuffer&& g);
 
  private:
-  std::vector<Variable> buffer;
+  std::vector<Variable> buffer; //存储梯度Variable
 };
 
 } // namespace autograd
